d1aec339a7850fc8b3e6e5d623adf94a23354d0e
matthieu.poirier <matthieu.poirier@believe.com>
2021-11-22 16:03:58 +0100

    first commit; add company entity

---
+++ contracts/BuildCollective.sol
@@ -0,0 +1,67 @@
+pragma solidity >=0.4.22 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+import "./Ownable.sol";
+
+contract BuildCollective is Ownable {
+  struct User {
+    string username;
+    uint256 balance;
+    bool registered;
+  }
+
+  struct Company {
+    string companyName;
+    uint256 balance;
+    User owner;
+    User[] employee;
+    bool registered;
+  }
+
+  mapping(address => User) private users;
+
+  event UserSignedUp(address indexed userAddress, User indexed user);
+
+  function user(address userAddress) public view returns (User memory) {
+    return users[userAddress];
+  }
+
+  function signUp(string memory username) public returns (User memory) {
+    require(bytes(username).length > 0);
+    users[msg.sender] = User(username, 0, true);
+    emit UserSignedUp(msg.sender, users[msg.sender]);
+  }
+
+  function addBalance(uint256 amount) public returns (bool) {
+    require(users[msg.sender].registered);
+    users[msg.sender].balance += amount;
+    return true;
+  }
+  mapping(string => Company) private companies;
+  mapping(address => Company) private employee;
+
+event CreateCompany(string indexed companyName, User indexed owner, Company indexed comp);
+
+  function createCompany(string memory companyName) public returns (Company memory) {
+    if(companies[companyName].registered == false){
+      companies[companyName].registered = true;
+      companies[companyName].name = companyName;
+      companies[companyName].owner = User.username;
+      companies[companyName].balance = 0;
+    }
+    emit CreateCompany(companyName, User.username, companies[companyName]);
+
+    return companies[companyName];
+  }
+
+ function addEmployee(string memory companyName, address newEmployee) public returns (Company memory){
+    require(users[msg.sender].registered); // If user aldready registred
+    require(users[newEmployee].registered); // If user aldready registred
+    require(companies[companyName].registered); // If company aldready registred
+    require(compareString(companies[companyName].owner.username, users[msg.sender].username)); // If user is company's owner
+
+    members[newEmployee] = companies[companyName];
+  }
+
+
+}
---
+++ src/views/EntrepriseAccount.vue
@@ -0,0 +1,132 @@
+<template lang="html">
+  <div class="entreprise" v-if="!account">
+    <form @submit.prevent="signUp">
+      <card
+        title="Enter your wallet address here"
+        subtitle="Type directly in the input and hit enter."
+      >
+        <input
+          type="text"
+          class="input-entreprise"
+          v-model="entreprise"
+          placeholder="Name your entreprise"
+        />
+        <input
+          type="text"
+          class="input-wallet"
+          v-model="username"
+          placeholder="Type your wallet address here"
+        />
+      </card>
+    </form>
+  </div>
+  <div class="entreprise" v-if="account">
+    <div class="card-entreprise-wrapper">
+      <card
+        :title="account.username"
+        :subtitle="`${balance} Ξ\t\t${account.balance} Tokens`"
+        :gradient="true"
+      >
+        <div class="explanations">
+          This data has been fetched from the blockchain. You started by
+          connecting MetaMask, and you fetched your data by reading the
+          blockchain. Try to modify the code to see what's happening!
+        </div>
+        <div class="explanations">
+          On your account on the contract, you have
+          {{ account.balance }} tokens. If you click
+          <button class="button-link" @click="addTokens">here</button>, you can
+          add some token to your account. Just give it a try! And think to put
+          an eye on Ganache!
+        </div>
+      </card>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { useStore } from 'vuex'
+import Card from '@/components/Card.vue'
+
+export default defineComponent({
+  components: { Card },
+  setup() {
+    const store = useStore()
+    const address = computed(() => store.state.account.address)
+    const balance = computed(() => store.state.account.balance)
+    const contract = computed(() => store.state.contract)
+    return { address, contract, balance }
+  },
+  data() {
+    const account = null
+    const username = ''
+    return { account, username }
+  },
+  methods: {
+    async updateAccount() {
+      const { address, contract } = this
+      this.account = await contract.methods.user(address).call()
+    },
+    async signUp() {
+      const { contract, username } = this
+      const name = username.trim().replace(/ /g, '_')
+      await contract.methods.signUp(name).send()
+      await this.updateAccount()
+      this.username = ''
+    },
+    async addTokens() {
+      const { contract } = this
+      await contract.methods.addBalance(200).send()
+      await this.updateAccount()
+    },
+  },
+  async mounted() {
+    const { address, contract } = this
+    const account = await contract.methods.user(address).call()
+    if (account.registered) this.account = account
+  },
+})
+</script>
+
+<style lang="css" scoped>
+.entreprise {
+  padding: 24px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 500px;
+  margin: auto;
+}
+
+.explanations {
+  padding: 12px;
+}
+
+.button-link {
+  display: inline;
+  appearance: none;
+  border: none;
+  background: none;
+  color: inherit;
+  text-decoration: underline;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+}
+
+.input-username {
+  background: transparent;
+  border: none;
+  padding: 12px;
+  outline: none;
+  width: 100%;
+  color: white;
+  font-family: inherit;
+  font-size: 1.3rem;
+}
+</style>
